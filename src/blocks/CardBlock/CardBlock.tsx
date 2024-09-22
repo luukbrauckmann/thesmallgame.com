2496833ed15636df21ccff4ec30a740745a1af97
Luuk <me@luuk.network>
2024-09-22 17:11:36 +0200

    Added meta tags

--- src/blocks/CardBlock/CardBlock.tsx
+++ src/blocks/CardBlock/CardBlock.tsx
@@ -1,4 +1,4 @@
-import { StructuredText } from "@components";
+import { StructuredText } from "react-datocms";
 
 interface Props {
   title: string;
@@ -9,7 +9,7 @@ const CardBlock = ({ title, body }: Props) => {
   return (
     <article>
       <h3>{title}</h3>
-      <StructuredText {...body} />
+      <StructuredText data={body} />
     </article>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 export { default as LocaleSelector } from './LocaleSelector/LocaleSelector';
 export { default as MainFooter } from './MainFooter/MainFooter';
 export { default as MainHeader } from './MainHeader/MainHeader';
-export { default as StructuredText } from './StructuredText/StructuredText';
